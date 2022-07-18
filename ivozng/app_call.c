13728f0fed6585007228cd39de0fe9ce692fcef8
Kaian <kaian@irontec.com>
2022-07-18 09:24:05 +0200

    call: increase call command optional parameter length

--- ivozng/app_call.c
+++ ivozng/app_call.c
@@ -398,7 +398,7 @@ int
 call_exec(session_t *sess, app_t *app, const char *args)
 {
     char actionid[ACTIONID_LEN];
-    char exten[128], options[80];
+    char exten[128], options[1024];
 
     if (!session_test_flag(sess, SESS_FLAG_AUTHENTICATED)) {
         return NOT_AUTHENTICATED;
